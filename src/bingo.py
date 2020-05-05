3de70e96812759b1b2ca2e1723e365c06b8378b4
santiago <SantiagoNicolasRotman@gmail.com>
2020-05-05 12:40:16 -0300

    Cambio los test a /src/bingo.py

--- src/bingo.py
+++ src/bingo.py
@@ -1,8 +1,44 @@
-#Funcion carton: 0 celda desocupada, 1 celda ocupada
-def carton():
-    mi_carton = (
-        (1,0,0,1,1,0,1,0,1),
-        (0,1,0,1,0,1,1,1,1),
-        (0,0,1,0,1,1,0,1,0)
-    )
-    return mi_carton
+#Compruebo que la cantidad de celdas ocupadas sean meyor o igual a 15
+def no_menor_15(carton1):
+	contador = 0
+	for fila in carton1:
+		for celda in fila:
+			if celda != 0:
+				contador += 1
+	return contador >= 15
+
+#Compruebo que la cantidad de celdas ocupadas sean menor o igual a 15
+def no_mayor_15(carton1):
+	contador = 0
+	for fila in carton1:
+		for celda in fila:
+			if celda != 0:
+				contador += 1
+	return contador <= 15
+
+#Compruebo que las dimenciones sean 9x3.
+def dimensiones(carton1):
+	contador_x = 0
+	for fila in carton1:
+		contador_y = 0
+		contador_x += 1
+		for celda in fila:
+			contador_y += 1
+		if contador_y != 9:
+			return False
+	return contador_x == 3
+
+#Compruebo que ninguna columna este vacia
+def columnas_ocupadas(carton1):
+	contador = 0
+	for i in range(9):
+		contador = 0
+		if carton1[0][i] != 0:
+			contador += 1
+		if carton1[1][i] != 0:
+			contador += 1
+		if carton1[2][i] != 0:
+			contador += 1
+		if contador == 0:
+			return False
+		return True
--- test/test_celdas.py
+++ test/test_celdas.py
@@ -1,46 +1,35 @@
-from src.bingo import carton #Agrego la funcion carton de src/bingo.py
+from src.bingo import no_menor_15  #Agrego la funcion no_menor_15  de src/bingo.py
+from src.bingo import no_mayor_15 #Agrego la funcion no_mayor_15 de src/bingo.py
+from src.bingo import dimensiones #Agrego la funcion dimensiones de src/bingo.py
+from src.bingo import columnas_ocupadas #Agrego la funcion columnas_ocupadas de src/bingo.py
+
+#Funcion carton: 0 celda desocupada, 1 celda ocupada
+def carton():
+    mi_carton = (
+        (2,0 ,0 ,10,12,0 ,15,0 ,19),
+        (0,25,0 ,27,0 ,58,69,71,89),
+        (0,0 ,20,0 ,25,71,0 ,82 ,0)
+    )
+    return mi_carton
 
 #Compruebo que la cantidad de celdas ocupadas sean mayor o igual a 15
 def test_no_menor_15():
-	carton1 = carton()
-	contador = 0
-	for fila in carton1:
-		for celda in fila:
-			contador += celda
-	assert contador >= 15
+	assert no_menor_15(carton())
 
 #Compruebo que la cantidad de celdas ocupadas sean menor o igual a 15
 def test_no_mayor_15():
-	carton1 = carton()
-	contador = 0
-	for fila in carton1:
-		for celda in fila:
-			contador += celda
-	assert contador <= 15
+	assert no_mayor_15(carton())
 
-#Compruebo que las dimenciones sean 9x3.
+
+#Compruebo que las dimensiones sean 9x3.
 def test_dimensiones():
-	carton1 = carton()
-	contador_x = 0
-	for fila in carton1:
-		contador_y = 0
-		contador_x += 1
-		for celda in fila:
-			contador_y += 1
-		assert contador_y == 9
+	assert dimensiones(carton())
 
-	assert contador_x == 3
 
 #Compruebo que ninguna columna este vacia
 def test_columnas_ocupadas():
-	carton1 = carton()
-	contador = 0
-	for i in range(9):
-		contador = 0
-		contador += carton1[0][i]
-		contador += carton1[1][i]
-		contador += carton1[2][i]
-		assert contador > 0
+	assert columnas_ocupadas(carton())
+
 
 
 
