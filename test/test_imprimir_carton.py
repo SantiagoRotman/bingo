80542cab6ad319e1e7b12abea3abbb4017e8ccc7
santiago <SantiagoNicolasRotman@gmail.com>
2020-07-05 19:00:55 -0300

    Ahora, si

--- test/test_imprimir_carton.py
+++ test/test_imprimir_carton.py
@@ -1,5 +1,5 @@
 from src.bingo import imprimirCarton # agrego la funcion imprimirCarton de /src/bingo.py
-from test.test_celdas import carton # agrego la funcion que crea el carton de prueba
+from src.bingo import generar_carton # agrego la funcion que crea el carton de prueba
 
 def test_imprimirCarton():
-    assert imprimirCarton(carton())
+    assert imprimirCarton(generar_carton())
